f60311cf28bb57f07192e85dcd84f81bb2085b48
Julien-Fruteau <julien.fruteau@gmail.com>
2024-01-05 14:23:17 +1100

    chore(nvim/dap-js): add loglevel

--- nvim/lua/plugins/dap-js.lua
+++ nvim/lua/plugins/dap-js.lua
@@ -16,7 +16,7 @@ return {
 				debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
 				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
 				log_file_path = "(stdpath cache)/dap_vscode_js.log", -- Path for file logging
-				-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+				log_file_level = 20, -- Logging level for output to file. Set to false to disable file logging.
 				log_console_level = vim.log.levels.WARN, -- Logging level for output to console. Set to false to disable console output.
 			})
 			for _, language in ipairs({ "typescript", "javascript" }) do
