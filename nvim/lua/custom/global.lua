602ed235a248500ef9b10fc79151903c4d6e45c8
Julien-Fruteau <julien.fruteau@gmail.com>
2024-01-09 08:08:51 +1100

    fix(nvim/clipboard): use xsel and free yourself from trouble

--- nvim/lua/custom/global.lua
+++ nvim/lua/custom/global.lua
@@ -3,35 +3,39 @@ local g = vim.g -- for conciseness
 -- disable autoformat
 g.autoformat = false
 
--- wsl clipboard
-local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
-if in_wsl then
-	g.clipboard = {
-		name = "WslClipboard",
-		copy = {
-			["+"] = { "clip.exe" },
-			["*"] = { "clip.exe" },
-		},
-		paste = {
-			["+"] = { "nvim_paste" },
-			["*"] = { "nvim_paste" },
-		},
-		cache_enabled = true,
-	}
-end
+-- cliptboard
+-- nb: wsl or linux just install xsel, with apt package manager 
 
-local in_linux = vim.loop.os_uname().sysname ~= "Linux"
-if in_linux then
-  g.clipboard = {
-		name = "LinuxClipboard",
-		copy = {
-			["+"] = { "xclip" },
-			["*"] = { "xclip" },
-		},
-		paste = {
-			["+"] = { "xsel" },
-			["*"] = { "xsel" },
-		},
-		cache_enabled = true,
-}
-end
+--
+-- wsl clipboard
+-- local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+-- if in_wsl then
+-- 	g.clipboard = {
+-- 		name = "WslClipboard",
+-- 		copy = {
+-- 			["+"] = { "clip.exe" },
+-- 			["*"] = { "clip.exe" },
+-- 		},
+-- 		paste = {
+-- 			["+"] = { "nvim_paste" },
+-- 			["*"] = { "nvim_paste" },
+-- 		},
+-- 		cache_enabled = true,
+-- 	}
+-- end
+
+-- local in_linux = vim.loop.os_uname().sysname ~= "Linux"
+-- if in_linux then
+--   g.clipboard = {
+-- 		name = "LinuxClipboard",
+-- 		copy = {
+-- 			["+"] = { "xsel" },
+-- 			["*"] = { "xsel" },
+-- 		},
+-- 		paste = {
+-- 			["+"] = { "xsel" },
+-- 			["*"] = { "xsel" },
+-- 		},
+-- 		cache_enabled = true,
+-- }
+-- end
--- scripts/README.md
+++ scripts/README.md
@@ -1,5 +1,8 @@
 # scripts
 
+> NB: DEPRECATED. Install xsel in wsl distro, and clipboard just work out of the box
+
+
 - nvim_paste for wsl by [mitchellt](https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html)
 
 > NB: add scripts folder to your $PATH and make a symlink to main/scripts : 
@@ -7,3 +10,4 @@
 ```bash
 ln -s .dot-files/main/scripts scripts
 ```
+
