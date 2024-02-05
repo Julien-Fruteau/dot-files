4edef9e491b299d7b99bca5b70aa6e4299231e62
Julien-Fruteau <julien.fruteau@gmail.com>
2024-02-06 09:15:55 +1100

    refact(colorscheme): to catppuccin macchiato

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,47 @@
+-- catppuccin
 return {
-	"folke/tokyonight.nvim",
-	lazy = true,
-	priority = 1000,
-	opts = { style = "storm" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		priority = 1000,
+		opts = { flavour = "macchiato" }, -- latte, frappe, macchiato, mocha }
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
 
---
+-- -- tokyonight
 -- return {
--- 	"navarasu/onedark.nvim",
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = true,
 -- 	priority = 1000,
+-- 	opts = { style = "storm" },
+-- }
+
+-- -- onedark
+-- return {
+-- 	{ "navarasu/onedark.nvim", priority = 1000 },
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "onedark",
+-- 		},
+-- 	},
 -- }
 --
+
+-- -- kanagawa
+-- return {
+-- 	{ "rebelot/kanagawa.nvim", priority = 1000 },
+-- 	{
+-- 		"lazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "kanagawa",
+-- 		},
+-- 	},
+-- }
