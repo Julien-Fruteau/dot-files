c40a5734c60066086fa3bc05f065b8223f8a5d70
Julien-Fruteau <julien.fruteau@gmail.com>
2024-02-14 10:24:20 +1100

    fix(telescope): restore default lazy vim conf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,22 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 		"dawsers/telescope-file-history.nvim",
+		{
+			"ahmedkhalf/project.nvim",
+			opts = {
+				manual_mode = true,
+			},
+			event = "VeryLazy",
+			config = function(_, opts)
+				require("project_nvim").setup(opts)
+				require("lazyvim.util").on_load("telescope.nvim", function()
+					require("telescope").load_extension("projects")
+				end)
+			end,
+			keys = {
+				{ "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+			},
+		},
 	},
 	config = function()
 		local telescope = require("telescope")
