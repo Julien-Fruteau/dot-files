a9cdaf0a4202ef76c8270709c0e2a171b57dd4b5
Julien-Fruteau <julien.fruteau@gmail.com>
2024-02-22 15:56:31 +1100

    style

--- nvim/lua/plugins/dap-js.lua
+++ nvim/lua/plugins/dap-js.lua
@@ -2,25 +2,25 @@ return {
 	{
 		"microsoft/vscode-js-debug",
 		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-    lazy = true,
+		lazy = true,
 	},
 	{
 		"mxsdev/nvim-dap-vscode-js",
-    lazy = true,
+		lazy = true,
 		requires = {
 			"mfussenegger/nvim-dap",
 			"vscode-js-debug",
 		},
-    -- opts = {
-    --   debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-    -- }
+		-- opts = {
+		--   debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+		-- }
 		config = function()
-      local dap = require('dap')
+			local dap = require("dap")
 			require("dap-vscode-js").setup({
 				node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
 				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-        -- debugger_cmd = { 'js-debug-adapter' },
-				adapters = {  "pwa-node" }, -- which adapters to register in nvim-dap
+				-- debugger_cmd = { 'js-debug-adapter' },
+				adapters = { "pwa-node" }, -- which adapters to register in nvim-dap
 				-- log_file_path = "(stdpath cache)/dap_vscode_js.log", -- Path for file logging
 				-- log_file_level = vim.log.levels.DEBUG,-- Logging level for output to file. Set to false to disable file logging.
 				-- log_console_level = vim.log.levels.TRACE -- Logging level for output to console. Set to false to disable console output.
