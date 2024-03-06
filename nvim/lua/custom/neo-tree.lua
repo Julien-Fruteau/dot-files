5f13a796da9053b0289810ec5518ce1dfabd29b2
Julien-Fruteau <julien.fruteau@gmail.com>
2024-03-06 16:00:52 +1100

    fix(conf/neo-tree): include default values

--- nvim/lua/custom/neo-tree.lua
+++ nvim/lua/custom/neo-tree.lua
@@ -1,10 +1,13 @@
 require("neo-tree").setup({
 	filesystem = {
+		bind_to_cwd = false,
+		follow_current_file = { enabled = true },
+		use_libuv_file_watcher = true,
 		filtered_items = {
-      -- visible = true,
+			-- visible = true,
 			hide_dotfiles = false,
-      hide_gitignored = false,
-      hide_hidden = false
+			hide_gitignored = false,
+			hide_hidden = false,
 		},
 	},
 })
