072f9132070d1691a76c282d1a5aa82fb7880c57
Julien-Fruteau <julien.fruteau@gmail.com>
2024-09-03 19:07:41 +1100

    chore(noice): add some messages to skip

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,13 +1,21 @@
 return {
-	-- messages, cmdline and the popupmenu
-		"folke/noice.nvim",
-		opts = function(_, opts)
-			table.insert(opts.routes, {
-				filter = {
-					event = "notify",
-					find = "No information available",
-				},
-				opts = { skip = true },
-			})
-    end
+	"folke/noice.nvim",
+	opts = function(_, opts)
+		table.insert(opts.routes, {
+			filter = { event = "notify", find = "No information available" },
+			opts = { skip = true },
+		})
+		table.insert(opts.routes, {
+			filter = { error = true, find = "Pattern not found" },
+			opts = { skip = true },
+    })
+		table.insert(opts.routes, {
+			filter = { error = true, find = "kotlin_language_server" },
+			opts = { skip = true },
+		})
+    table.insert(opts.routes, {
+      filter = { warning = true, find = "Client jdtls quit with exit code 1 and signal 0" },
+      opts = { skip = true },
+    })
+	end,
 }
