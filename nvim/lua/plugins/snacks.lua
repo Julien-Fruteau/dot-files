61b1587215a1ea38266df8e10d8010dbba0a71cc
Julien-Fruteau <julien.fruteau@gmail.com>
2025-12-11 14:50:57 +1100

    chore: snacks shows hidden files

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -9,6 +9,12 @@ return {
 		statuscolumn = { enabled = false }, -- we set this in options.lua
 		toggle = { map = LazyVim.safe_keymap_set },
 		words = { enabled = true },
+		picker = {
+			hidden = true,
+		},
+		explorer = {
+			hidden = true,
+		},
 	},
   -- stylua: ignore
   keys = {
